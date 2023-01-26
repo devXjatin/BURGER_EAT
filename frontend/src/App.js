02dc9fcdf9ff7c7e6431e4aff4ef0f4dbcc55d84
devXjatin <jatin13280@gmail.com>
2023-01-26 20:24:42 +0530

    cart created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home"
 import Header from "./components/layout/Header"
 import Footer from "./components/layout/Footer"
 import Contact from "./components/contact/Contact"
+import Cart from "./components/cart/Cart"
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/Home.scss"
@@ -10,6 +11,7 @@ import "./styles/Founder.scss"
 import "./styles/Menu.scss"
 import "./styles/Footer.scss"
 import "./styles/Contact.scss"
+import "./styles/Cart.scss"
 
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/contact" element={<Contact/>}/>
+      <Route path="/cart" element={<Cart/>}/>
     </Routes>
     <Footer/>
    </Router>
