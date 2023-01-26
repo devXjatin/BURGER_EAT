7a86a70505298724451ac1c210e0a409f5fc6ff9
devXjatin <jatin13280@gmail.com>
2023-01-26 19:13:33 +0530

    contact form created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import Home from "./components/home/Home"
 import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
+import Contact from "./components/contact/Contact"
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/Home.scss"
 import "./styles/Founder.scss"
 import "./styles/Menu.scss"
 import "./styles/Footer.scss"
-import Footer from "./components/layout/Footer"
+import "./styles/Contact.scss"
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
     <Footer/>
    </Router>
