57e518264831253ffac66f0b127cfc59f901b12f
devXjatin <jatin13280@gmail.com>
2023-01-27 16:27:05 +0530

    logged in successfully

--- backend/api/v1/routes/user.js
+++ backend/api/v1/routes/user.js
@@ -3,8 +3,17 @@ import passport from "passport";
 
 const router = express.Router();
 
+//google oauth 2.0 route
 router.get("/google-oauth", passport.authenticate("google",{
     scope:["profile"]
 }))
+
+//login route 
+router.get("/login", passport.authenticate("google",{
+    scope:["profile"],
+    successRedirect:process.env.FRONTEND_URL
+}))
+
+
 
 export default router;
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import {connectPassport} from "./utils/Provider.js"
+import session from 'express-session';
 
 const app = express();
 export default app
@@ -10,8 +11,17 @@ dotenv.config({
 })
 
 //middleware
-connectPassport()
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
 
+app.use(passport.authenticate("session"))
+app.use(passport.initialize());
+app.use(passport.session())
+
+connectPassport()
 
 app.get('/', (req, res)=>{
     res.send("<h1>WORKING!!!</>")
@@ -19,5 +29,6 @@ app.get('/', (req, res)=>{
 
 // Importing routes
 import userRoute from "./api/v1/routes/user.js";
+import passport from 'passport';
 
 app.use("/api/v1", userRoute);
