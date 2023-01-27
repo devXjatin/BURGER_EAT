1c526acd48ebee904387a6865fc8fdb480d19cc6
devXjatin <jatin13280@gmail.com>
2023-01-27 16:17:02 +0530

    google oauth20 setup

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import {connectPassport} from "./utils/Provider.js"
 
 const app = express();
 export default app
@@ -7,3 +8,16 @@ export default app
 dotenv.config({
     path:"./env/config.env"
 })
+
+//middleware
+connectPassport()
+
+
+app.get('/', (req, res)=>{
+    res.send("<h1>WORKING!!!</>")
+})
+
+// Importing routes
+import userRoute from "./api/v1/routes/user.js";
+
+app.use("/api/v1", userRoute);
