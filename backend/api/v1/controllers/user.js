eddca48a2601654cd77c09a93c53028bcc693335
devXjatin <jatin13280@gmail.com>
2023-02-01 15:00:59 +0530

    admin stats

--- backend/api/v1/controllers/user.js
+++ backend/api/v1/controllers/user.js
@@ -1,5 +1,6 @@
 import {asyncError} from "../middlewares/errorMiddleware.js"
 import {User} from "../models/User.js"
+import {Order} from "../models/order.js"
 
 //get my profile business logic
 export const getMyProfile = (req, res, next)=>{
@@ -30,5 +31,34 @@ export const getAdminUsers = asyncError(async(req, res, next)=>{
     res.status(200).json({
         success:true,
         users
+    })
+})
+
+//get admin stats
+export const getAdminStats = asyncError(async(req, res, next)=>{
+    const usersCount = await User.countDocuments();
+    
+    const orders = await Order.find({})
+
+    const preparingOrders = orders.filter(i=>i.orderStatus === "Preparing")
+    const shippedOrders = orders.filter(i=>i.orderStatus === "Shipped")
+    const deliveredOrders = orders.filter(i=>i.orderStatus === "Delivered")
+    
+    let totalIncome = 0;
+
+    orders.forEach(item=>{
+        totalIncome+=item.totalAmount;
+    })
+
+    res.status(200).json({
+        success:true,
+        usersCount,
+        ordersCount:{
+            total:orders.length,
+            preparing:preparingOrders.length,
+            shipped:shippedOrders.length,
+            delivered:deliveredOrders.length,
+        },
+        totalIncome
     })
 })
